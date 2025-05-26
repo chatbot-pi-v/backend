7794490322ac78f94db27b1568509962b4663200
brenno <martiniano.brenno@gmail.com>
2025-05-25 23:31:06 -0300

    feat: update response

--- src/modules/controllers/question.controller.ts
+++ src/modules/controllers/question.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
-import { QuestionResponse } from '../types/question';
 import { QuestionClient } from '../services/question-api.client';
+import { FirebaseService } from 'src/firebase/firebase.service';
+import { QuestionResponse } from '../types/question';
 
 @Controller()
 export class QuestionController {
-  constructor(private readonly question: QuestionClient) {}
+  constructor(
+    private readonly question: QuestionClient,
+    private readonly firebaseService: FirebaseService
+  ) {}
 
   @Post('/question')
   @HttpCode(200)
@@ -13,6 +17,24 @@ export class QuestionController {
       throw new Error('Nenhuma pergunta recebida');
     }
 
-    return await this.question.sendQuestionClient(question);
+    try {
+      const response = await this.question.sendQuestionClient(question);
+      console.log('response = ', response)
+
+      const answer = response.answer;
+      
+      const questionData = {
+        question,
+        answer,
+        timestamp: new Date(),
+      };
+
+      await this.firebaseService.createDocument('questions', questionData);
+
+      return response;
+    } catch (error) {
+      console.error('Erro ao processar pergunta:', error);
+      throw error;
+    }
   }
 }
